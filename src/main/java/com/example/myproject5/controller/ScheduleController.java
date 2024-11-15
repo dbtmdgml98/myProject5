3b154fff0d49f617c0862d410cf92061ee214e93
dbtmdgml98 <dbtmdgml98@naver.com>
2024-11-15 12:20:55 +0900

    Feat: 특정 Schedule 조회
    Lv 1. 일정 CRUD

--- src/main/java/com/example/myproject5/controller/ScheduleController.java
+++ src/main/java/com/example/myproject5/controller/ScheduleController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +38,14 @@ public class ScheduleController {
         List<ScheduleResponseDto> scheduleResponseDtoList = scheduleService.findAll();
 
         return new ResponseEntity<>(scheduleResponseDtoList, HttpStatus.OK);
+    }
+
+    // 특정 일정 조회
+    @GetMapping("/{id}")
+    public ResponseEntity<ScheduleResponseDto> findById(@PathVariable Long id) {
+
+        ScheduleResponseDto scheduleResponseDto = scheduleService.findById(id);
+
+        return new ResponseEntity<>(scheduleResponseDto, HttpStatus.OK);
     }
 }
--- src/main/java/com/example/myproject5/service/ScheduleService.java
+++ src/main/java/com/example/myproject5/service/ScheduleService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +37,16 @@ public class ScheduleService {
         // 조회된 이 Schedule 리스트를 ScheduleResponseDto 리스트 형태로 반환 받을 수 있게 한다.
        // List<Schedule> findAllSchedule = scheduleRepository.findAll();
         return scheduleRepository.findAll().stream().map(ScheduleResponseDto::toDto).toList();
+    }
+
+    public ScheduleResponseDto findById(Long id) {
+
+        // Schedule Entity가 아닌 Optional 형태로 반환이 되므로
+        // Optional<Schedule> optionalSchedule = scheduleRepository.findById(id);
+
+        // scheduleRepository 내에서 default로 선언한 메서드를 사용하여 바로 Schedule Entity로 반환받는다! (id를 통해 특정 게시물 조회)
+        Schedule findSchedule = scheduleRepository.findByIdOrElseThrow(id);
+
+        return new ScheduleResponseDto(findSchedule.getId(), findSchedule.getToDoTitle(), findSchedule.getToDoContents());
     }
 }
